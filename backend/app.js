3e0f1196409bc2e4cf944b88a419be1e9fed9f86
gastineeaujulie <gastineeaujulie@gmail.com>
2026-02-06 18:50:56 +0100

    installation package cors mongoos et dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,40 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI)
+ .then(() => console.log('Connexion à MongoDB réussie !'))
+ .catch(err => console.error('Connexion à MongoDB échoué !', err));
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('ok ok ok');
-    next();
-});
 
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
+app.use(cors());
+app.use(express.json());
 
-app.use((req, res, next) => {
-    res.json({ message: 'ok !'});
-    next();
-});
 
-app.use((req, res) => {
-    console.log('Reponse ok !');
-})
+// app.use('/api/books', (req, res, next) => {
+//    const books =  [
+//     {
+
+//     }]
+// });
+
+// app.use('/api/books/:id', (req, res, next) => {
+//     res.status(201);
+//     next();
+// });
+
+// app.use((req, res, next) => {
+//     res.json({ message: 'ok !'});
+//     next();
+// });
+
+// app.use((req, res) => {
+//     console.log('Reponse ok !');
+// })
 
 module.exports = app;
